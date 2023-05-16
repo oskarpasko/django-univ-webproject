b77f40ee60ddeddb15a6c7247d4e65801fcb0200
Oskar <>
2023-05-16 12:15:23 +0200

    create view with databse output

--- phone_service/record/views.py
+++ phone_service/record/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, "record/index.html")
+    clients = Client.objects.all()
+    employees = Employee.objects.all()
+    posts = Post.objects.all()
+    services = Service.objects.all()
+    records = Record.objects.all()
+    context = {
+            'clients':clients,
+            'employees':employees,
+            'posts':posts,
+            'services':services,
+            'records':records
+            }
+    return render(request, "record/index.html", context)
