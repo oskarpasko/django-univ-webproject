d612f08356a59f593c8afe14f746fd86091ed52f
Oskar <oskarpasko@gmail.com>
2023-06-08 21:33:04 +0200

    add some functions to the employee table

--- phone_service/record/admin.py
+++ phone_service/record/admin.py
@@ -12,7 +12,7 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = Client
     list_display = ("email", "first_name", "last_name", "phone",)
-    list_filter = ("email", "phone", "is_staff", "is_active",)
+    list_filter = ("is_staff", "is_active",)
     fieldsets = (
         (None, {"fields": ("email", "password", "first_name", "last_name", "phone")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
@@ -29,9 +29,21 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ("email", "phone")
     ordering = ("email",)
 
+class EmployeeAdmin(admin.ModelAdmin):
+    model = Employee
+    list_filter = ["email", "phone", "posn__name"]
+    list_display = ["email", "first_name", "last_name", "phone", "get_posn"]
+    search_fields = ["email", "phone", "last_name", "first_name"]
+    ordering = ["email",]
+
+    def get_posn(self, obj):
+        return obj.posn.name
+    get_posn.admin_order_field  = 'posn'
+    get_posn.short_description = 'position' 
+
 
 admin.site.register(Client, CustomUserAdmin)
-admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Posn)
 admin.site.register(Service)
 admin.site.register(Record)
