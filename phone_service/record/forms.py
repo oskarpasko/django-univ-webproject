e361a5653cb9483454d350b37d3d3b84587a41cd
eltwor <43753759+eltwor@users.noreply.github.com>
2023-06-18 00:35:21 +0200

    create register form

--- phone_service/record/forms.py
+++ phone_service/record/forms.py
@@ -2,6 +2,25 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from .models import Client
 
+class RegisterForm(UserChangeForm):
+
+    class Meta:
+        model=Client
+        fields = ['first_name', 'last_name', 'phone','email','password1','password2']
+
+    email = forms.CharField(min_length=6, max_length=65, required=True, widget=forms.TextInput(
+                            attrs={'placeholder': 'Your Email',
+                                    'type': 'email'}))
+    first_name = forms.CharField(min_length=1, max_length=50, required=True, widget=forms.TextInput(
+                            attrs={'placeholder': 'Your First Name'}))
+    last_name = forms.CharField(min_length=1,max_length=50, required=True, widget=forms.TextInput(
+                            attrs={'placeholder': 'Your Last Name'}))
+    phone = forms.CharField(min_length=9, max_length=9, required=False, widget=forms.TextInput(
+                            attrs={'placeholder': 'Your Phone'}))
+    password1 = forms.CharField(min_length=8, max_length=65, required=True, widget=forms.PasswordInput(
+                            attrs={'placeholder': 'Your Password'}))
+    password2 = forms.CharField(min_length=8, max_length=65, required=True, widget=forms.PasswordInput(
+                            attrs={'placeholder': 'Repeat Password'}))
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -17,7 +36,7 @@ class CustomUserChangeForm(UserChangeForm):
         fields = ("email", "first_name", "last_name", "phone")
         
 class LoginForm(forms.Form):
-    email = forms.CharField(max_length=65, required=True, widget=forms.TextInput(
+    email = forms.CharField(min_length=6, max_length=65, required=True, widget=forms.TextInput(
                             attrs={'class': "form-control"}))
-    password = forms.CharField(max_length=65, widget=forms.PasswordInput(
+    password = forms.CharField(min_length=6, max_length=65, widget=forms.PasswordInput(
                             attrs={'class':   "form-control"}))
