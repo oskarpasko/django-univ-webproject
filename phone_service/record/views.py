100debdde033f2105733baccd6e8656be54098a8
Oskar <oskarpasko@gmail.com>
2023-06-27 09:25:40 +0200

    change layout of main website

--- phone_service/record/views.py
+++ phone_service/record/views.py
@@ -22,21 +22,9 @@ def register(request):
             return render(request, 'record/register.html', {'form': form})
 
 def index(request):
-    clients = Client.objects.all()
-    employees = Employee.objects.all()
-    posts = Posn.objects.all()
-    services = Service.objects.all()
-    records = Record.objects.all()
     locations = Location.objects.all()
-    context = {
-            'clients':clients,
-            'employees':employees,
-            'posts':posts,
-            'services':services,
-            'records':records,
-            'locations':locations
-            }
-    return render(request, "record/index.html", context)
+
+    return render(request, "record/index.html", {"locations":locations})
 
 def sign_in(request):
 
